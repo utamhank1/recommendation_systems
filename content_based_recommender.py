4a2f6c13d99615cf5a64a8d091c619304a5268fb
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-03-23 16:44:40 -0400

    Code for predicting the most similar car to a given set of user specifications using the Nearest Neighbors algorithm

---
+++ content_based_recommender.py
@@ -0,0 +1,22 @@
+# Import libraries.
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+
+
+def main():
+    cars = pd.read_csv('mtcars.csv')
+    cars.columns = ['car_names', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'weight', 'qsec', 'vs', 'am', 'gear', 'carb']
+    print(cars.head())
+    t = [15, 300, 160, 3.2]
+
+    X = cars.iloc[:, [1, 3, 4, 6]].values
+    print(X[0:5])
+
+    nbrs = NearestNeighbors(n_neighbors=1).fit(X)
+    print(nbrs.kneighbors([t]))
+
+    print(f"The {cars['car_names'][22]} is the most similar car to the specifications the user specified.")
+
+
+if __name__ == "__main__":
+    main()
